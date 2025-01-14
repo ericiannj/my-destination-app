307063d5b79003c91a9150bc18ffc356f466b8d5
ericiannj <ericiannj@gmail.com>
2025-01-14 19:03:17 -0300

    feat(Map): implement click effect

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -10,21 +10,32 @@ interface MapProps {
   mapStyle?: string;
   width?: string;
   height?: string;
+  handleChangeCoordinates: (coordinates: Coordinates) => void;
 }
+
+export type Coordinates = [number, number] | null;
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN || '';
 
 const Map: React.FC<MapProps> = ({
-  initialCenter = [-74.006, 40.7128], // Default to NYC
+  initialCenter = [-74.006, 40.7128], //NYC
   initialZoom = 12,
   mapStyle = 'mapbox://styles/mapbox/streets-v12',
   width = '100%',
   height = 'calc(100vh - 64px)',
+  handleChangeCoordinates,
 }) => {
   const mapContainer = useRef<HTMLDivElement>(null);
   const map = useRef<mapboxgl.Map | null>(null);
   const [mapLoaded, setMapLoaded] = useState(false);
-  console.log('mapLoaded:', mapLoaded);
+
+  const handleMapClick = useCallback(
+    (e: mapboxgl.MapMouseEvent) => {
+      const coordinates: [number, number] = [e.lngLat.lng, e.lngLat.lat];
+      handleChangeCoordinates(coordinates);
+    },
+    [handleChangeCoordinates],
+  );
 
   useEffect(() => {
     if (!mapContainer.current || map.current) return;
@@ -39,17 +50,33 @@ const Map: React.FC<MapProps> = ({
     map.current.addControl(new mapboxgl.NavigationControl(), 'bottom-left');
     map.current.addControl(new mapboxgl.FullscreenControl(), 'bottom-left');
 
+    map.current.on('click', handleMapClick);
+
     map.current.on('load', () => {
       setMapLoaded(true);
     });
 
     return () => {
       if (map.current) {
+        map.current.off('click', handleMapClick);
         map.current.remove();
         map.current = null;
       }
     };
-  }, [initialCenter, initialZoom, mapStyle]);
+  }, []);
+
+  useEffect(() => {
+    if (map.current && mapLoaded) {
+      map.current.setStyle(mapStyle);
+    }
+  }, [mapStyle, mapLoaded]);
+
+  useEffect(() => {
+    if (map.current && mapLoaded) {
+      map.current.setCenter(initialCenter);
+      map.current.setZoom(initialZoom);
+    }
+  }, [initialCenter, initialZoom, mapLoaded]);
 
   return (
     <div
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,28 @@
 
 import dynamic from 'next/dynamic';
 import ClientOnly from './components/ClientOnly';
+import { useState } from 'react';
+import { Coordinates } from './components/Map';
 
 const MapWithNoSSR = dynamic(() => import('./components/Map'), { ssr: false });
 
 export default function Home() {
+  const [coordinates, setCoordinates] = useState<Coordinates>(null);
+  console.log('coordinates:', coordinates);
+
+  const handleChangeCoordinates = (coordinates: Coordinates) => {
+    setCoordinates(coordinates);
+  };
+
   return (
     <main className="min-h-screen p-8">
+      <h1>
+        {!!coordinates
+          ? `${coordinates[0]} | ${coordinates[1]}`
+          : 'Select Coordinates'}
+      </h1>
       <ClientOnly>
-        <MapWithNoSSR />
+        <MapWithNoSSR handleChangeCoordinates={handleChangeCoordinates} />
       </ClientOnly>
     </main>
   );
